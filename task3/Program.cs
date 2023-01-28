9fab4a79b37a5542159920ab2a6709200888acfd
Евгений <Grekos_89@mail.ru>
2023-01-28 15:04:01 +0300

    метод поиска цифры

---
+++ task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, -3] -> да
+
+
+
+int[] newArr = RandomArray(10);
+PrintArray(newArr);
+System.Console.WriteLine(GetFindNum(-2, newArr));
+
+bool GetFindNum(int num, int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(-9,10);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
